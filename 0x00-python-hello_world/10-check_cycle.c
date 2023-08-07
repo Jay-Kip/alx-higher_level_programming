35d3098b1fed026289e6f5937733c35587164ea9
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-07 11:55:46 +0000

    11. Hello, write

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - function to check if a linked list has a cycle in it
+ * @list: the list
+ * Return: 1 if present 0 if absent
+ */
+
+int check_cycle(listint_t *list)
+{
+
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,17 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/**
- * struct list_k - the single linked list
- * @n: Integer n
- * @next: pointer to the next node
- */
+#include <stdlib.h>
 
-typedef struct list_k
+/**
+ ** struct listint_s - singly linked list
+ ** @n: integer
+ ** @next: points to the next node
+ **
+ ** Description: singly linked list node structure
+ ** 
+ **/
+typedef struct listint_s
 {
 	int n;
-	struct list_k *next;
-} list_k;
-size_t print_listint(const listint_t *h);
+	struct listint_s *next;
+} listint_t;
 
-#endif
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
