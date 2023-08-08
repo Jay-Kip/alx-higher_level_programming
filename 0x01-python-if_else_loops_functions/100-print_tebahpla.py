e5b32dcb6359cfe605e5b48c06c2e0f7f9f9669f
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-08 19:05:16 +0000

    Reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
-for i range(65, 123):
+for i in range(65, 122):
+    if i >= 97 and i <= 122:
+        i = i - 32
+        i = chr(i)
+        print(i, end=", ")
+    elif i >= 65 and i <=96:
+        i = i + 32
+        i = chr(i)
+        print(i, end=", ")
+    else:
+        prin("")
 
