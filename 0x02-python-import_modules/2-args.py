93492caf9b9fbb8a051183d4aa92b9748a180cb5
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-10 18:10:14 +0000

    Prigram that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    length = len(args)
+    
+    if length == 0:
+        print("0 arguements.")
+
+    elif length == 1:
+        print("1 arguement:")
+    else:
+        print("{} arguements:".format(length))
+
+    for i in range(length):
+        print("{}: {}".format(i + 1, args[i]))
