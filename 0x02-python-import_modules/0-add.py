d961204ea70b5171281bb6aaa7b3f0b0b0e857c7
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-10 11:49:04 +0000

    Function that calls add function located in another file.Code deoesn'y exectute when imported using __import__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
-add = __import__('add_0').add
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-
-print(add(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+import calculator_1
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
