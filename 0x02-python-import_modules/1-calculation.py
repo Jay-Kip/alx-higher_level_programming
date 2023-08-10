0e9deab258bfb54202426e1e702aee9df56c0d8b
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-10 12:01:38 +0000

    Function that does all basic math operations by calling a function located in another file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import sys
-import calculator_1
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
