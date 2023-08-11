d63778051b1ba6d3dad3f2fb3d7c15ff173d0136
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-11 18:55:09 +0000

    Hidden discovery

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+    args = len(sys.argv[1:])
+
+    if op == '+':
+        print(op)
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+
+    elif op == '-':
+        print(op)
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+
+    elif op == "*":
+        print(op)
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+
+    elif op == '*':
+        print(op)
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+
+'''    if args != 3:
+        print("Usage: {} {} {} {}".format(args[0], args[1], args[2], args[3])) '''
