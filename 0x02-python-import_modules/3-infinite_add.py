e7e329e844936ed1ff2842353d870cc3fb6fc21f
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-11 19:01:02 +0000

    Function to add an infinite numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    arg = sys.argv[1:]
 
-arg = sys.argv[1:]
-
-total = 0
-for i in arg:
-    total += int(i)
-print("{}".format(int(total)))
+    total = 0
+    for i in arg:
+        total += int(i)
+    print("{}".format(int(total)))
