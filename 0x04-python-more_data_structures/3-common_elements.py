53603daa17961fb50e67c942dcb8f6133d74c56d
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-16 20:28:25 +0000

    a function that returns a set of common elements in two sets. Uses the {&} operator

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    return set_1 & set_2   #   The "&" operator is used to check common elements 
+    return set_1 & set_2  # The "&" operator is used to check
