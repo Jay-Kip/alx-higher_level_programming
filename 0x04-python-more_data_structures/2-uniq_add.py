cbdd8b03209992a6896dfcee8969eaafad379dd8
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-16 20:15:36 +0000

    a function that adds all unique integers in a list (only once for each integer). Uses set() to filter unique numbers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    uniq = set(my_list)   # The set() filters unique numbers
+
     add = 0
-    for i in my_list:
+
+    for i in uniq:
         add += i
-    return add
+    return (add)
