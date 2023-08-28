1f0668596bb2cf3cb3397518533ec9a3225b336e
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-28 23:51:27 +0000

    Function that uses exception handling to safely execute a function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return (fct(*args))
-    except:
+    except Exception as e:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
