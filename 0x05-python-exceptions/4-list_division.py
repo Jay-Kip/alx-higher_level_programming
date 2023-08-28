71ba8bac4f59616e9e7dc5c6306f53122e9537c0
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-28 11:59:31 +0000

    Function that uses exception handlinig to divide two numbers from different lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,13 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             division = my_list_1[i] / my_list_2[i]
-        except ZeroDivisionError:
+        except ZeroDivisionError:  # Handles zero division error
             print("division by 0")
             division = 0
-        except TypeError:
+        except TypeError:  # Handles typeError
             print("wrong type")
             division = 0
-        except IndexError:
+        except IndexError:  # Handles Index error
             print("out of range")
         finally:
             new.append(division)
