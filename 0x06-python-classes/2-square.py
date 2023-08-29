3c9de597a2c6c27e2828120eaa5014d736b10c4d
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-29 22:09:49 +0000

    Class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin.python3
+
+'''Class Aquare that defines a square by: (based on 1-square.py)'''
+
+
+class Square:
+    def __init__(self, size=0):
+
+        '''Isinstance is a bulit-in Python function'''
+        '''Here it checks if size is int'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
