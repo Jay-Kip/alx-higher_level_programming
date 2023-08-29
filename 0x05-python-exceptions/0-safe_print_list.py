755610b4d2b4f8caae2271fd9e8f59aa28c06cde
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-29 00:17:30 +0000

    Function that uses exception to print x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import dis
 
 
 def safe_print_list(my_list=[], x=0):
@@ -12,5 +11,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return (count)
-
-dis.dis(safe_print_list)
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 res += (a ** b) / i
-        except:
+        except Exception as e:
             res = b + a
             break
         return (res)
