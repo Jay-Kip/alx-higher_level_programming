ac3043b82da784449e4ef6e4c4bc819b3cb287a6
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-29 00:37:23 +0000

    Majic calculation from python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    res = 0
+    result = 0
 
-    for i in range(1, 3):
+    for i in range(0, 2):
         try:
             if i > a:
                 raise Exception('Too far')
             else:
                 res += a ** b / i
         except Exception as e:
-            res = b + a
+            result = b + a
             break
-    return (res)
+    return (result)
