398880b29bc85cd876b2f53bf94ee608e4b0d74a
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-09-09 00:53:00 +0000

    Clear String

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+'''
+a function that prints a text with 2 new lines after each of these characters: ., ? and :
+'''
+
 
 def text_indentation(text):
+    '''Function that indents text'''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     result = ""
