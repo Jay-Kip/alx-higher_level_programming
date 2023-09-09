e10767748fe195883e7b349c38799dbd0468dc0f
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-09-09 00:45:08 +0000

    Clear string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,7 @@ a function that prints My name is <first name> <last name>
 
 
 def say_my_name(first_name, last_name=""):
+    '''Function that recieves first and last name and prints out full names'''
     if type(first_name) not in [str]:
         raise TypeError('first_name must be a string')
     if type(last_name) not in [str]:
