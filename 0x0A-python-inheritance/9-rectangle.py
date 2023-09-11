23dfb3e91c3e83d6eb221fe2a74ee187ace9b803
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-09-11 22:18:45 +0000

    a class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,23 +6,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     '''class Rectangle'''
     def __init__(self, width, height):
+        super().integer_validator("width", width)
         self.__width = width
-        self.__height = height
-
-    def width(self):
-        '''sets width'''
-        return self.__width
-
-    def width(self, value):
-        '''Sets / gets height'''
-        self.__width = value
-
-    def height(self):
-        '''sets height'''
-        return self.__height
-
-    def height(self, value):
-        '''sets / gets height'''
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
