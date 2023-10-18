6012b9b1fef674d05794082b2c6328b9bb8730f8
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-18 05:28:23 +0300

    SCript that computes avarage score of all records

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
 -- Computes the avarage score of all records in the table 'second_table'
-SELECT AVG(`score`) AS `avarage` FROM `second_table`;
+SELECT AVG(`score`) AS `avarage`
+FROM `second_table`;
