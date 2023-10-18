a077706d16c632c4e166d42a9c0bf16aecd93a7a
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-18 05:44:38 +0300

    Script that lists all records except ones with no name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records except ones with no names
-SELECT * FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
