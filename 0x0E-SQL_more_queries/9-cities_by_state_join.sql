2cd3147f5953372f526b7684caa565dd914b136f
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-19 00:58:06 +0000

    Lists all cities contained in the database'

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists all cities contaied in the database
-SELECT id, name FROM cities;
+SELECT states.id = cities.state_id RIGHT JOIN cities ON cities.id, cities.name, states.name FROM cities ORDER BY cities.id ASC;
