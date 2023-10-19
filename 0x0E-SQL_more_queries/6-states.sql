43f4803a9d8f26bf8cb17be6cdd3633e72ee2614
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-19 00:26:31 +0000

    Script that creates a database and a table with auto-generated id'

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Creates a database 'hbtn_0d_usa' and a table 'states'
 -- Create Database first
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use the created database
+USE hbtn_0d_usa;
 -- Create table
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL,
 PRIMARY KEY (id));
