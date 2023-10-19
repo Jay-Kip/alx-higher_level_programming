66b8d50970ea4db05e4f5fe6192fc1d346f21df6
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-19 00:49:10 +0000

    Lists all cities of Califonia that could be found in the database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- Lists all the cities of califonia that can be found in the database
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states where name = 'Califonia') ORDER BY id ASC;
+
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states where name = 'California') ORDER BY id ASC;
