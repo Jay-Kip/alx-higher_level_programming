d8c8d41f28947fabc6e503de58a65498e2260761
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-19 00:08:15 +0000

    Script that creates a table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- Creates a table called 'force_name'
 -- 'force_name' description 'id'-> INT 'name'->VARCHAR
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
