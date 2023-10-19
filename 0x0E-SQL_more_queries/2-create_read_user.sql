c78282e72993c914ca1670d30ebf448f0ff330b6
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-10-19 00:02:48 +0000

    Script that creates a database called 'hbtn_0d_2' and user 'user_0d_2' and gives SELECT privilage to the user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Creates a database called '2-create_read_user.sql' and the user 'user_0d_2'
 -- Creating database first
-CRAETE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Now creating the user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- Now giving SELECT privillage to user
