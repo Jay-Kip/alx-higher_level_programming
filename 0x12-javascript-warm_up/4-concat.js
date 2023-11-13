fc1610f42ebdedfde117e914eed082cc0f763f5f
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-13 17:45:50 -0600

    a script that prints two arguments passed to it, in the following format: ' is'

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const [_, __, arg1, arg2] = process.argv;
-console.log(`${arg1} is ${arg2}`);
+/* const [_, __, arg1, arg2] = process.argv; */
+console.log(process.argv[2] + ' is ' + process.argv[3]);
