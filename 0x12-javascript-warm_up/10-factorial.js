9c50e34c67d43ff35c62c7e0309ea8df002e3b96
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-13 19:47:47 -0600

    A script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,9 +4,9 @@ function factorial (x) {
   if (x < 0) {
     return (-1);
   }
-  	if (!x || isNaN(x)) {
-    		return (1);
-  	}
-  	return (x * factorial(x - 1));
+  if (!x || isNaN(x)) {
+    	return (1);
+  }
+  return (x * factorial(x - 1));
 }
 console.log(factorial(Number(process.argv[2])));
