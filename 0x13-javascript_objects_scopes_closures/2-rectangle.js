5713314ad96ec1ced8372af58cf9c5cfb7465155
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-14 16:56:43 -0600

    A class Rectangle that defines a rectangle and If w or h is equal to 0 or not a positive integer, create an empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-	  if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+	  if (w <= 0 || h <= 0 || !Number(w) || !Number(h)) {
 		  return 'Rectangle {}';
 	  }
     this.width = w;
