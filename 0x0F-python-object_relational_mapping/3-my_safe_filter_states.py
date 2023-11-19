937f04ba83ad7137fd19836d53f103f46916cab2
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-20 01:09:23 +0300

    A script safe from SQL-injection that takes user input as argument to dispalay contents in the database

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''A secure script which takes in argument'''
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    '''Establish a connection to the database'''
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    '''Create a cursor'''
+    cursor = db.cursor()
+
+    '''Excecute SQL commands'''
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                   .format(sys.argv[4], ))
+
+    '''Fetch all the rows retured by the query'''
+    rows  = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
