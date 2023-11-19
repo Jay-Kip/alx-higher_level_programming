58cbb2abb869eda29273c7cc5a532a76af24c7e3
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-20 02:37:29 +0300

    Contains the class definition of a State and an instance Base = declarative_base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
 '''Defines class state which inherits from Base'''
-from sqlalchemy import MetaData, Column, Integer, String
+
+from sqlalchemy import  Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mt = MetaData()
 Base = declarative_base(metadata=mt)
 
 
-classState(Base):
+class State(Base):
     """Declares Class State"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
