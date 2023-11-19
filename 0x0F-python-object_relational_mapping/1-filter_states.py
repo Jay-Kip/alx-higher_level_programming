22e840972f3170681468fbc3526e3bf701a81540
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-19 23:19:17 +0300

    A script that lists all states with a name starting with N (upper N) from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,16 +8,16 @@ import MySQLbd
 if __name__ == "__main__":
     '''Establish a connection to the database'''
     db = MyQSLdb.connect(host="localhost",
-                       user=sys.argv[1],
-                       passwd=sys.argv[2],
-                       db=sys.argv[3],
-                       port=3306)
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
 
     '''Create cursor'''
     cursor = db.cursor()
     '''execute SQL query'''
     cursor.execute("""SELECT * FROM states
-                   WHERE name LIKE BINARY 'N%'
+                   WHERE name LIKE 'N%'
                    OREDER BY states.id""")
 
     '''Fetching all the rows returned by the query'''
