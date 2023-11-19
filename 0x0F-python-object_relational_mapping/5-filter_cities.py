715ec36d7103b58f1fb85de02c5f8cac415fad2a
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-20 02:08:18 +0300

    A script that takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,10 +16,14 @@ if __name__ == "__main__":
     '''Create a Cursor'''
     cursor = db.cursor()
 
-    '''Excecute SQL statements'''
+    '''Excecute SQL statements
     cursor.execute("""SELECT cities.name FROM cities INNER JOIN
                    states ON states.id=cities.state_id
-                   WHERE states.name=%s""", (sys.argv[4], ))
+                   WHERE states.name=%s""", (sys.argv[4], ))'''
+
+    cursor.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
 
     '''Fetch all the rows returned by the query'''
     rows = cursor.fetchall()
