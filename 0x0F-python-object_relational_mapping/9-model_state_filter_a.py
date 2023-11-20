0c9134ec9ec8b2575ff7fcbf8426bcfe07dd355d
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-20 03:59:12 +0300

    A script that lists all State objects that contain the letter a from the database

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,12 +7,17 @@ from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
+    '''Creating database engine'''
     engine = create_engine('mysql+mysqldb://{}: {}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
+    '''Creating database tables'''
     Base.metadata.create_all(engine)
+
+    '''creates session'''
     Session - sessionmaker(bind=engine)
     session = Session()
 
+    '''Queryin the database and printing result'''
     for instance in session.query(State).filter(State.name.like('%a%')):
         print(instance.id, instance.name, sep=": ")
