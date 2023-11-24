1bcec53ed7ef92ab7476e590373740ec14efe65b
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-25 02:45:13 +0300

    A script that filters states

---
+++ 0x0F-python-object_relational_mapping/11-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Lists all states with name starting with {N}"""
+import sys
+import MySQLbd
+
+
+if __name__ == "__main__":
+    '''Establish a connection to the database'''
+    db = MyQSLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    '''Create cursor'''
+    cursor = db.cursor()
+    '''execute SQL query'''
+    cursor.execute("""SELECT * FROM states
+                   WHERE name LIKE 'N%'
+                   OREDER BY states.id""")
+
+    '''Fetching all the rows returned by the query'''
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
