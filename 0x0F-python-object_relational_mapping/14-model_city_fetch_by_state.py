dfd328fc2210663b015a641eb37eca819183be1b
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-25 02:49:33 +0300

    prints all City objects from the database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in (session.querry(State.name, City.id, City.name)
+    for instance in (session.query(State.name, City.id, City.name)
                      .filter(State.id == City.state_id)):
         print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
