c98b1cbfaee8872975f6d5e7424bd886609d8c23
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-12-02 01:09:46 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,14 @@
 '''Fetches "https://alx-intranet.hbtn.io/status" '''
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-    html = resp.read()
-    print("Body response :")
-    print(f"\t- type : {type(html)}")
-    print(f"\t- content : {html}")
-    print(f"\t- utf8 content : {html.decode()}")
+def main():
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+        html = resp.read()
+        print("Body response:")
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode()}")
+
+if __name__ == "__main__":
+    main()
