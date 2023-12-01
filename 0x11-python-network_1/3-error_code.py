8bb50059f511f7d0e3652d3633589f8d26aba094
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-12-02 02:45:27 +0300

    Handling HTTP errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''manage urllib.error.HTTPError exceptions
+and print: Error code: followed by the HTTP status code
+'''
+import sys
+import urllib.request
+
+
+def main():
+    '''Handles HTTP Errors'''
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as resp:
+        try:
+            html =resp.read()
+            print(html)
+
+        except urllib.error.HTTPError as e:
+            print(f"HTTP Error: {e.code} {e.reason}")
+            # Handle the HTTP error as needed
