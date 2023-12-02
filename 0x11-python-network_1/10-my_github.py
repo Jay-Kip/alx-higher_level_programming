4bcc698098227e90f3317929f87f46247b443b94
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-12-02 05:12:36 +0300

    Python script that takes github username and personal access token and uses GitHub API to display id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Uses github API to display my id'''
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def main():
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+
+    response = requests.get('https://api.github.com/user',
+                            auth=HTTPBasicAuth(username, passwd))
+
+    if response.status_code == 200:
+        try:
+            profile = response.json()
+            print(profile['id'])
+
+        except ValueError:
+            print("None")
+
+
+if __name__ == "__main__":
+    main()
