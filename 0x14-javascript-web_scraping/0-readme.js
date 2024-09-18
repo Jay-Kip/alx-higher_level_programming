8a7dacf1e5c83f60d7ecdf241ae899d62c8daa57
Jay-Kip <hanningtonkiptoo14@gmail.com>
2024-09-18 11:32:08 +0300

    a script that reads and prints the content of a file using require package.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+fs.readFile(filename, 'utf-8', function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
